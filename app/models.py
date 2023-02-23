c0d1da89f1a450d700a4ec7bde4269bfe3f49dfb
BANOUN Tahar <taharbanoun06@gmail.com>
2023-02-23 16:18:16 +0100

    creation d'un header et la page mes projets

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Projet(models.Model):
+    titre = models.CharField(max_length=30)
+    url = models.CharField(max_length=50)
+    
--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from app.models import Projet
 
 # Create your views here.
+
+def hello(self):
+    pro = Projet.objects.all()
+    return render(self,'app/mes_projets.html',{'pr':pro})
+
+
+def test(self):
+    return render(self,'app/nav_bar.html')
